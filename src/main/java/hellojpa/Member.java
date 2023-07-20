21dafa3466ee338fec2fcd954f99b88cff2b3ae7
jun <ggprgrkjh@naver.com>
2023-07-20 22:53:19 +0900

    feat: 객체와 테이블 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,8 +3,10 @@ package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="MBR")
 public class Member {
 
     @Id
