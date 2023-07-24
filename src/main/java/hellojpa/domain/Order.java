d468162d59eb447b50cc1588245aba491a5518aa
jun <ggprgrkjh@naver.com>
2023-07-24 20:42:07 +0900

    feat: Item Entity

--- src/main/java/hellojpa/domain/Order.java
+++ src/main/java/hellojpa/domain/Order.java
@@ -4,15 +4,13 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "ORDERS")
 public class Order {
 
     @Id @GeneratedValue
     private Long id;
-
     private Long memberId;
-
     private LocalDateTime orderDate;
-
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
