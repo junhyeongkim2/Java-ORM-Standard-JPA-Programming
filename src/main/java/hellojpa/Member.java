a793820fef29af2a762c15fc0b1e9c86bc9b8f13
jun <ggprgrkjh@naver.com>
2023-07-24 20:37:17 +0900

    feat: Member Entity

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="MBR")
@@ -13,7 +10,9 @@ public class Member {
     private Long id;
     private String name;
 
+    @Enumerated(EnumType.STRING)
     private RoleType roleType;
+
 
     public Long getId() {
         return id;
