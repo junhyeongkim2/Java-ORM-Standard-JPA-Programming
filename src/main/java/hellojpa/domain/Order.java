0af2ea7f15c3edd6796bdec29361b897c315d871
jun <ggprgrkjh@naver.com>
2023-07-24 20:39:46 +0900

    feat: Order Entity

--- src/main/java/hellojpa/domain/Order.java
+++ src/main/java/hellojpa/domain/Order.java
@@ -1,4 +1,19 @@
 package hellojpa.domain;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
 }
