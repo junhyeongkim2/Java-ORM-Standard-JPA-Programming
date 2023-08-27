b7f255d53ad7f9c4cc2075b1e0350129a6c7d2d3
jun <ggprgrkjh@naver.com>
2023-08-27 22:05:03 +0900

    feat: Category Entity

--- src/main/java/hellojpa/domain/Category.java
+++ src/main/java/hellojpa/domain/Category.java
@@ -1,4 +1,26 @@
 package hellojpa.domain;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+
+
+
+
 }
